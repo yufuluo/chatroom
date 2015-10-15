b4b5b614e358d8271d4f04d68d8660a00f22eab7
Kay Lau <qijunliu7@gmail.com>
2015-10-15 16:04:40 -0700

    final

--- public/Javascripts/chat.js
+++ public/Javascripts/chat.js
@@ -7,10 +7,14 @@ Chat.prototype.sendMessage = function(room, text) {
 		room: room,
 		text: text
 	};
+	this.socket.emit('message', message);
+}
+
+Chat.prototype.changeRoom = function(room) {
 	this.socket.emit('join', {
 		newRoom: room
 	});
-};
+}
 
 Chat.prototype.processCommand = function(command) {
 	var words = command.split(' ');
